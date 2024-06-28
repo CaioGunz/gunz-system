66442e5b9187e272b8c435fc80c58dcdf91c4fa9
CaioGunz <caiobarbosa2404@hotmail.com>
2024-06-28 17:34:27 -0300

    Criacao da primeira pagina do sistema com o titulo ja formatado

--- main.py
+++ main.py
@@ -3,13 +3,25 @@ import customtkinter
 import requests
 
 
-def main():
-    root = customtkinter.CTk()
-    root.resizable(width=None, height=None)
-    root.geometry("500x300")
-    
-    
-    root.mainloop()
-    
-if __name__ == '__main__':
-    main()
+
+class Janelas(customtkinter.CTk):
+    def __init__(self):
+        super().__init__()
+        
+        # Geração e configuração da tela principal do sistema
+        self.resizable(width=None, height=None)
+        self.geometry("500x300")
+        # Configuração Icone
+        self.after(200, lambda: self.iconbitmap('assets/logoGrande-40x40.ico'))
+        self.title('Gunz System Finance')
+        
+        # Configurar colunas da grade
+        self.grid_columnconfigure(0, weight=1)
+        self.grid_columnconfigure(1, weight=0)
+        self.grid_columnconfigure(2, weight=1) 
+        
+        self.tituloJanelaPrincipal = customtkinter.CTkLabel(self, text="Sistema de Controle Financeiro", font=('Arial', 16, 'bold'))
+        self.tituloJanelaPrincipal.grid(column=1, row=1, columnspan=1, pady=10) 
+
+app = Janelas()
+app.mainloop()
