d505a81320842144c80ef861acb4058ac37f77eb
CaioGunz <caiobarbosa2404@hotmail.com>
2024-06-28 11:59:05 -0300

    Começo do sistema de financas Gunz

---
+++ main.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import customtkinter
+import requests
+
+
+def main():
+    root = customtkinter.CTk()
+    root.resizable(width=None, height=None)
+    root.geometry("500x300")
+    
+    
+    root.mainloop()
+    
+if __name__ == '__main__':
+    main()
