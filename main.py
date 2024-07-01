e27854fecf4a5a2ac8b458818611aa91da10e763
CaioGunz <caiobarbosa2404@hotmail.com>
2024-07-01 17:41:48 -0300

    Criação das Labels e Entrys da classe faculdade para coleta de dados

--- main.py
+++ main.py
@@ -34,23 +34,64 @@ class JanelaFaculdade(customtkinter.CTkToplevel):
         self.parent = parent
         self.janelaIncial = janelaInicial
         self.resizable(width=None, height=None)
-        self.geometry('500x300')
+        self.geometry('900x300')
         # Configuracao Icone
         self.after(200, lambda: self.iconbitmap('assets/logoGrande-40x40.ico'))
         self.title('Gunz System - Faculdade')
         
-        # Configurar colinas da grade para centralizar
-        self.grid_columnconfigure(0, weight=1)
-        self.grid_columnconfigure(1, weight=0)
-        self.grid_columnconfigure(2, weight=1)
-        
         # Titulo pagina faculdade - Princial
         self.tituloPrincipalFaculdade = customtkinter.CTkLabel(self, text='Notas e Mensalidade Faculdade', font=('Montserrat', 20))
-        self.tituloPrincipalFaculdade.grid(column=1, row=1, columnspan=1, pady=10)
+        self.tituloPrincipalFaculdade.place(relx=0.5, rely=0.05, anchor='center')
+        
+        # Abre espaco vazio para organizar 
+        self.vazio = customtkinter.CTkLabel(self, text='')
+        self.vazio.grid(column=0, row=1, pady=10)
+        
+        # Label e Entry para coleta do nome da materia
+        self.labelNomeMateria = customtkinter.CTkLabel(self, text='Entre com o nome da matéria:', font=('Montserrar', 14))
+        self.labelNomeMateria.grid(column=0, row=2, padx=10, pady=5)
+        self.entryNomeMateria = customtkinter.CTkEntry(self, placeholder_text='Nome Matéria', width=200, border_color='#008485')
+        self.entryNomeMateria.grid(column=1, row=2, pady=5)
+
+        # Label e Entry para coleta da Nota Atividade 1
+        self.labelAtividade1 = customtkinter.CTkLabel(self, text='Entre com a Nota Atividade 1:', font=('Montserrar', 14))
+        self.labelAtividade1.grid(column=0, row=3, padx=10, pady=5)
+        self.entryAtividade1 = customtkinter.CTkEntry(self, placeholder_text='Ex: 0,5', width=200, border_color='#008485')
+        self.entryAtividade1.grid(column=1, row=3, pady=5)
+
+        # Label e Entry para coleta da Nota Atividade 2
+        self.labelAtividade2 = customtkinter.CTkLabel(self, text='Entre com a Nota Atividade 2:', font=('Montserrar', 14))
+        self.labelAtividade2.grid(column=0, row=4, padx=10, pady=5)
+        self.entryAtividade2 = customtkinter.CTkEntry(self, placeholder_text='Ex: 0,5', width=200, border_color='#008485')
+        self.entryAtividade2.grid(column=1, row=4, pady=5)
+
+        # Label e Entry para coleta da Nota Atividade 3
+        self.labelAtividade3 = customtkinter.CTkLabel(self, text='Entre com a Nota Atividade 3:', font=('Montserrar', 14))
+        self.labelAtividade3.grid(column=0, row=5, padx=10, pady=5)
+        self.entryAtividade3 = customtkinter.CTkEntry(self, placeholder_text='Ex: 0,5', width=200, border_color='#008485')
+        self.entryAtividade3.grid(column=1, row=5, pady=5)
+
+        # Label e Entry para coleta da Nota Atividade 4
+        self.labelAtividade4 = customtkinter.CTkLabel(self, text='Entre com a Nota Atividade 4:', font=('Montserrar', 14))
+        self.labelAtividade4.grid(column=0, row=6, padx=10, pady=5)
+        self.entryAtividade4 = customtkinter.CTkEntry(self, placeholder_text='Ex: 0,5', width=200, border_color='#008485')
+        self.entryAtividade4.grid(column=1, row=6, pady=5)
+
+        # Label e Entry para coleta da Nota MAPA
+        self.labelAtividade4 = customtkinter.CTkLabel(self, text='Entre com a Nota MAPA', font=('Montserrar', 14))
+        self.labelAtividade4.grid(column=2, row=2, padx=10, pady=5)
+        self.entryAtividade4 = customtkinter.CTkEntry(self, placeholder_text='Ex: 0,5', width=200, border_color='#008485')
+        self.entryAtividade4.grid(column=3, row=2, pady=5)
+
+        # Label e Entry para coleta da Nota Atividade 4
+        self.labelAtividade4 = customtkinter.CTkLabel(self, text='Entre com a Nota SGC:', font=('Montserrar', 14))
+        self.labelAtividade4.grid(column=2, row=3, padx=10, pady=5)
+        self.entryAtividade4 = customtkinter.CTkEntry(self, placeholder_text='Ex: 0,5', width=200, border_color='#008485')
+        self.entryAtividade4.grid(column=3, row=3, pady=5)
         
         # Botão para voltar à janela inicial
         self.botaoVoltarInicial = BotaoVoltarInicial(self, janelaInicial, font=('Montserrat', 14, 'bold'), fg_color='#054648', hover_color='#003638')
-        self.botaoVoltarInicial.grid(column=1, row=2, columnspan=1, pady=10)
+        self.botaoVoltarInicial.grid(column=1, row=7, pady=10)
         
         # Chama a função universal para perguntar se o usuario realmente deseja fechar o sistema
         self.protocol('WM_DELETE_WINDOW', lambda: fechajanelasSecundarias(self, self.parent))
